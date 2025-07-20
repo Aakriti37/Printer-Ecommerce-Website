ea00de6c7ba19cfe36339acaade1427f0d6f8991
Aakriti37 <akritichaudhary964@gmail.com>
2025-07-20 13:25:29 +0545

    First Commit

---
+++ Product-Page/productjs.js
@@ -0,0 +1,5 @@
+// A function to toggle filter box 
+function toggleFilterBox() {
+            var filterBox = document.getElementById('filterBox');
+            filterBox.style.display = (filterBox.style.display === 'none' || filterBox.style.display === '') ? 'block' : 'none';
+}
